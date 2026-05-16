013ab64327578c1a7359d86d7022a74519d68dcc
meetkhetani2003 <meetkhetani1111@gmail.com>
2026-05-16 18:43:51 +0530

    added

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -4,6 +4,11 @@ import work3 from './surface_geometric_01.jpeg'
 import work4 from './surface_material_01.jpeg'
 import work5 from './surface_folio_01.jpeg'
 import work6 from './surface_sculptural_01.jpeg'
+import pebble_1 from './pebble_1.jpeg'
+import pebble_2 from './pebble_2.jpeg'
+import pebble_3 from './pebble_3.jpeg'
+import pebble_4 from './pebble_4.jpeg'
+import pebble_5 from './pebble_5.jpeg'
 import logo from './MK Creation.png'
 
 export {
@@ -13,5 +18,10 @@ export {
     work4,
     work5,
     work6,
+    pebble_1,
+    pebble_2,
+    pebble_3,
+    pebble_4,
+    pebble_5,
     logo
 }
